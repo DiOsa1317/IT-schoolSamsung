3cf345b6675cead7a61d3d07d681f6ce2878b7fa
DiOsa1317 <114155374+DiOsa1317@users.noreply.github.com>
2022-10-04 16:49:06 +0300

    29.09.2022
    Попробуем, что выйдет

---
+++ Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+/*byte aByte=-128;
+short aShort=32767;
+int aInt=2000000000;
+long aLong=2000000000000000000L;
+double aDouble=8.33333;
+float aFloat=8.34585999F;
+boolean aBoolean=3<4;
+char aChar='a';
+        System.out.println((char)(aChar+1));
+        System.out.println((char) 97 +1);
+        System.out.println((char) 97.894949);
+        System.out.println((int)(97.564784));
+//Scanner sc= new Scanner(System.in);
+        //System.out.println(aByte);
+        //System.out.println(aShort);
+       // System.out.println(aInt);
+        System.out.println(aBoolean);*/
+
+        /*int a=11, b=5;
+        //int res;
+        double res;
+        res=a+b;
+        System.out.println(res);
+        res=a-b;
+        System.out.println(res);
+        res=a*b;
+        System.out.println(res);
+        res=a/b; //целочисленное деление
+        System.out.println(res);
+        res=(a+0.0)/b;
+        System.out.println(res);
+        res=(double) a/b;
+        System.out.println(res);
+        System.out.println(res);
+        res=(double) (a/b);
+        System.out.println(res);
+        res=a%b;
+        System.out.println(res);
+
+    }
+}
+*/
+        int i = 0;
+        System.out.println(i++);
+        System.out.println(i);
+        i--;
+        System.out.println(++i); //i=i+1
+        System.out.println(i--);//i=i-1;
+        System.out.println(i);
+        i = 10;
+        System.out.println(i);
+        i += 5;
+        System.out.println(i);
+        i /= 2;
+        System.out.println(i);
+        i %= 7;
+        System.out.println(i);
+        i *= 9;
+        System.out.println(i);
+    }
+}
