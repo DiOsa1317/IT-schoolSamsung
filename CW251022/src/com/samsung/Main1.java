d90e5ffc3ae058385042730d305711998f2c4a41
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-26 20:34:42 +0300

    task15

---
+++ CW251022/src/com/samsung/Main1.java
@@ -0,0 +1,18 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        for(int i=0; i<a.length; i=i+2) {
+            for(int j=0; j<a[i].length; j=j+2) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
