feabb6f55be107b7599bc7975a8d7e127d12e88f
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-11-01 20:29:32 +0300

    beginning

---
+++ test011122oop/src/com/samsung/model/Person.java
@@ -0,0 +1,10 @@
+package com.samsung.model;
+
+public class Person {
+    String name;
+    int age;
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
