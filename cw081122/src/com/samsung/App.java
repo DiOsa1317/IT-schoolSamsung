8bb24e72dfa69b8f55bb12ee060b43175b61fd30
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-11-09 22:46:41 +0300

    beginning

---
+++ cw081122/src/com/samsung/App.java
@@ -0,0 +1,7 @@
+package com.samsung;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ cw081122/src/com/samsung/model/Product.java
@@ -0,0 +1,12 @@
+package com.samsung.model;
+
+public class Product {
+    private String name;
+    private int cost;
+    private int count;
+
+    public String getName() {
+        return name;
+    }
+
+}
